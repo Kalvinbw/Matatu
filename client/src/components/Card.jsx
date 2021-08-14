cdc5dc1dd150c726b268280fabab7db9b4106495
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-08-14 13:28:52 -0600

    Added svg

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import logo from '../assets/logo.svg';
+import heart from '../assets/hearts.svg'
+import club from '../assets/clubs.svg'
+import diamond from '../assets/diamonds.svg'
+import spade from '../assets/spades.svg'
 
 function Card(props) {
     function onClick() {
         props.handleChange(props.card);
+    }
+
+    let myImg;
+    switch(props.card.suit) {
+        case 'Heart':
+            myImg = heart;
+            break;
+        case 'Club':
+            myImg = club;
+            break;
+        case 'Diamond':
+            myImg = diamond;
+            break;
+        case 'Spade':
+            myImg = spade;
+            break;
+        default:
+            myImg = logo;
+            break;
     }
 
     let myClass = props.className;
@@ -18,7 +41,7 @@ function Card(props) {
         return (
             <div className={myClass} onClick={onClick}>
                     <h6>{props.card.suit}</h6>
-                    <img src={logo} alt='react logo'></img>
+                    <img src={myImg} alt='react logo'></img>
                     <h6>{props.card.number}</h6>
             </div>
         );
