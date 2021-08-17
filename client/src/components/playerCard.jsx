58b2eb3cfef1d5511c787db82392b48bd54ef7a1
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-08-17 12:57:47 -0600

    avatar

--- client/src/components/playerCard.jsx
+++ client/src/components/playerCard.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+
 
 function PlayerCard(props) {
-    let myClass = props.className;
-    myClass = myClass + ' flipped';
-    if(props.player.turn) {
-        myClass = myClass + ' selected';
-    }
-    return (
-        <div className={myClass}>
-            <h6>{props.player.name}</h6>
-            <h6>Cards: {props.player.cards.length}</h6>
-            <h6>Score: {props.player.score}</h6>
-        </div>
-    );
+  let myClass = props.className;
+  myClass = myClass + ' flipped';
+
+  if(props.player.turn) {
+      myClass = myClass + ' selected';
+  }
+
+  return (
+      <div className={myClass}>
+          <Avatar>{props.player.name}</Avatar>
+          <h6>Cards: {props.player.cards.length}</h6>
+          <h6>Score: {props.player.score}</h6>
+      </div>
+  );
 }
 
 export default PlayerCard;
