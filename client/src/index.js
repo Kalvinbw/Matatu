88e48501d3b902243ba0da52b30a2e1e76189fbf
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-09-28 09:14:13 -0600

    implement google firestore

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './App';
 import MyApp from './MyApp';
 import reportWebVitals from './reportWebVitals';
 
